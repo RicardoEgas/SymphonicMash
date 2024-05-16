10c71ca5b84f737cf8bc39f82848ecaf454492a5
Graciano Henrique <gracianomanuelhenrique@gmail.com>
2024-05-16 02:38:43 +0100

    add the description elements

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,16 @@
 import profileStyle from '../assets/css/profile.module.css';
+import user from '../assets/img/user.svg';
 
 const Profile=()=>{
     return(
-        <div className={profileStyle.body}>
-            <h1>Iam the profile...</h1>
+        <div className={profileStyle.profileContainer}>
+            <div className={profileStyle.avatarContainer}>
+            <img src={user} alt="Purnima Khan"/>
+            </div>
+            <div className={profileStyle.description}>
+            <h3>Purnima Khan</h3>
+            <p>Guidhali School of Music and Drama</p>
+            </div>
         </div>
     );
 };
