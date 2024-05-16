ef0997ba51b215a44e3758e2bff17115ef1712fd
Graciano Henrique <gracianomanuelhenrique@gmail.com>
2024-05-17 00:14:05 +0100

    add the audio action

--- src/components/ProfileAudio.jsx
+++ src/components/ProfileAudio.jsx
@@ -1,9 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faX } from '@fortawesome/free-solid-svg-icons'
+import { faX,faPlay,faPause } from '@fortawesome/free-solid-svg-icons'
 import profileStyle from '../assets/css/profile.module.css';
 import user from '../assets/img/user.svg';
+import { useState } from 'react';
+import music from '../assets/music/music.mp3';
 
 const ProfileAudio=()=>{
+    const [playIcon,setPlayIcon]=useState(faPlay);
+    const [audio,setAudio]=useState(null);
     return(
         <div className={profileStyle.profileContainer}>
             <div className={profileStyle.avatarContainer}>
@@ -15,40 +19,28 @@ const ProfileAudio=()=>{
             <div className={profileStyle.audioFileContainer}>
                 <div className={profileStyle.audioItem}>
                 <div>
-                   <div></div> <p>Sent From Even</p> 
-                </div>
-                <button className={profileStyle.audioItemDeleteButton}>
-                <FontAwesomeIcon icon={faX}/>
-                </button>
-                <button className={profileStyle.audioItemUpdateButton}>
-                UPDATE
-                </button>
-                </div>
-                <div className={profileStyle.audioItem}>
-                <div>
-                   <div></div> <p>Sent From Even</p> 
-                </div>
-                <button className={profileStyle.audioItemDeleteButton}>
-                <FontAwesomeIcon icon={faX}/>
-                </button>
-                <button className={profileStyle.audioItemUpdateButton}>
-                UPDATE
-                </button>
-                </div>
-                <div className={profileStyle.audioItem}>
-                <div>
-                   <div></div> <p>Sent From Even</p> 
-                </div>
-                <button className={profileStyle.audioItemDeleteButton}>
-                <FontAwesomeIcon icon={faX}/>
-                </button>
-                <button className={profileStyle.audioItemUpdateButton}>
-                UPDATE
-                </button>
-                </div>
-                <div className={profileStyle.audioItem}>
-                <div>
-                   <div></div> <p>Sent From Even</p> 
+                   <div>
+                    <button onClick={()=>{
+                        if(!audio){
+                            let audioItem=document.createElement('audio');
+                            audioItem.src=music;
+                            audioItem.play();
+                            setAudio(audioItem);
+                            setPlayIcon(faPause);
+                        }else{
+                            if(audio.paused) {
+                                audio.play();
+                                setPlayIcon(faPause);
+                            } else {
+                                audio.pause();
+                                setPlayIcon(faPlay);
+                            }
+                        }
+                    }
+                    }>
+                   <FontAwesomeIcon icon={playIcon}/>
+                    </button>
+                    </div> <p>Sent From Even</p> 
                 </div>
                 <button className={profileStyle.audioItemDeleteButton}>
                 <FontAwesomeIcon icon={faX}/>
